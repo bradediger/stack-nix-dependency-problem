c31d728f7188d5ec843ebbd4d83d2141c10b65c9
Brad Ediger <brad.ediger@madriska.com>
2016-12-14 15:09:52 -0600

    any extra-libraries will do

--- shell.nix
+++ shell.nix
@@ -1,13 +1,9 @@
 let
   pkgs = import ./nixpkgs {}; # nixos-16.09
-  libxlsxwriter = import ./libxlsxwriter.nix { inherit pkgs; };
-
 in pkgs.haskell.lib.buildStackProject {
   name = "problem";
   ghc = pkgs.haskell.compiler.ghc801;
   buildInputs = with pkgs; [
-    libxlsxwriter gmp zlib
+    gmp zlib
   ];
-
-  LD_LIBRARY_PATH = [libxlsxwriter];
 }
